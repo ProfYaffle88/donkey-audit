47a608c9c4629914f47ad698226356615d42d5f5
James Price <james.a.t.price@gmail.com>
2024-02-05 15:32:22 +0000

    Removed call for selectMp() and replaced with selectMpById(iD) for testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 /* UK Parliament API */
-const API_URL = "https://members-api.parliament.uk"
-const API_SEARCH = "/api/Members/Search"
+const API_URL = "https://members-api.parliament.uk";
+const API_SEARCH = "/api/Members/Search";
+const API_MEMBERS = "/api/Members"
 
  /*Find MP ID by search*/
  async function selectMp() {
     // let searchName = new FormData(document.getElementById("checksform"));
-    let searchName = "Jacob Rees Mogg"
+    // let searchName = "Mogg"
     const searchString = `${API_URL}${API_SEARCH}?Name=${searchName}`
     const response = await fetch(searchString);
     const data = await response.json();
@@ -19,9 +20,26 @@ const API_SEARCH = "/api/Members/Search"
         throw new Error(data.error);
     }
 }
-selectMp();
+
 /* Get MP data by /{id}/ */
+async function selectMpById(iD) {
+    const mpById = `${API_URL}${API_MEMBERS}/${iD}`;
+    const response = await fetch(mpById);
+    const data = await response.json();
 
+    if (response.ok) {
+        console.log(data);
+        displayData(data);
+    } else {
+        console.log(data);
+        displayException(data);
+        throw new Error(data.error);
+    }
+}
 
+selectMpById(4099);
 
 /* Display MP data */
+function displayData() {
+
+}
